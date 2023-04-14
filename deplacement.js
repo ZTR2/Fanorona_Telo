5bd05d37ddef5869a04dc87b7b82fe3fda47584e
ZTR2 <ramahefarisonthierry@gmail.com>
2023-04-14 19:38:31 +0300

    Version alpha

--- deplacement.js
+++ deplacement.js
@@ -83,7 +83,6 @@ function moveCercle(id, mouseX, mouseY) {
                 for(let j2 = 0; j2 <= 8; j2++) {
                     if (succ[i2][j2]) {
                         if(getNumeroInvI(j2)==posi && getNumeroInvJ(j2)==posj){
-                            console.log("Deplacement autorisé");
                             shape.x = line.tab[posi][posj].x;
                             shape.y = line.tab[posi][posj].y;
                             shape.i = posi;
@@ -91,9 +90,9 @@ function moveCercle(id, mouseX, mouseY) {
 
                             if (testAlign(posi, posj, shape.colorOriginal)) {
                                 if(tour == noir)
-                                    alert("Noir a gagné. FELICITATIONS !");
+                                    document.getElementById("winner-message").innerHTML = "Noir a gagné. FELICITATIONS !";
                                 else if(tour == vert)
-                                    alert("Vert a gagné. FELICITATIONS !");
+                                    document.getElementById("winner-message").innerHTML = "Vert a gagné. FELICITATIONS !";
                                 gagne = true;
                             }
 
@@ -115,7 +114,6 @@ function moveCercle(id, mouseX, mouseY) {
 }
 
 function testAlign(i, j, couleur) {
-    console.log("i: "+i+", j: "+j);
     if(shapes.length >= 3) {
         let i2 = getNumero(i, j);
         let i3, j3, difI, difJ;
@@ -123,12 +121,10 @@ function testAlign(i, j, couleur) {
             if(succ[i2][j2]) {
                 i3 = getNumeroInvI(j2);
                 j3 = getNumeroInvJ(j2);
-                console.log("Succ => i: "+i3+", j: "+j3);
                 for(let shape of shapes) {
                     if(shape.i == i3 && shape.j == j3 && shape.colorOriginal == couleur) {
                         difI = i3 - i;
                         difJ = j3 - j;
-                        console.log("dif => "+difI+", "+difJ);
                         if(difI>=0 && difJ>=0) {
                             if(i3+difI<=2 && j3+difJ<=2) {
                                 for(let sh of shapes) {
@@ -184,4 +180,61 @@ function testAlign(i, j, couleur) {
         }
     }
     return false;
+}
+
+function getPosition(mouseX, mouseY) {
+    let distance;
+    let ptX;
+    let ptY;
+    let p;
+    for (let i = 0; i <= 2; i++) {
+        for (let j = 0; j <= 2; j++) {
+            ptX = line.tab[i][j].x;
+            ptY = line.tab[i][j].y;
+            distance = distanceEntreCoordonnees(mouseX, mouseY, ptX, ptY);
+            if(distance <= 100) {
+                p = new position(i, j);
+                return p;
+            }
+        }
+        
+    }
+}
+
+function verifiOccupe(posi, posj) {
+    if(shapes.length > 0){
+        for(let shape of shapes) {
+            if(shape.i == posi && shape.j == posj) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function posePiece(mouseX, mouseY) {
+    // i, j du click
+    let pos = getPosition(mouseX, mouseY);
+    if(!verifiOccupe(pos.i, pos.j)) {
+        let id = "piece"+ nbrPieces;
+        if(parite(nbrPieces) && !testAlign(pos.i, pos.j, noir)) {
+            shapes.push(new pions(id, pos.i, pos.j, noir));
+            nbrPieces++;
+        } else if(!parite(nbrPieces) && !testAlign(pos.i, pos.j, vert)) {
+            shapes.push(new pions(id, pos.i, pos.j, vert));
+            if(nbrPieces == 5)
+                document.getElementById("help").innerHTML = "Maintenant, selectionnez une pièce et déplacez-la. Noir Commence !";
+            nbrPieces++;
+        } else {
+            if(parite(nbrPieces)) {
+                document.getElementById("winner-message").innerHTML = "Noir a gagné. FELICITATIONS !";
+            }
+            else {
+                document.getElementById("winner-message").innerHTML = "Vert a gagné. FELICITATIONS !";
+            }
+            gagne = true;
+        }
+    } else {
+        document.getElementById("help").innerHTML = "Cette position est déjà occupée";
+    }
 }
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+document.getElementById("winner").style.display = "none";
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var height = canvas.height;
@@ -37,57 +39,9 @@ let posi;
 let posj;
 let nbrPieces = 0
 
-function getPosition(mouseX, mouseY) {
-    let distance;
-    let ptX;
-    let ptY;
-    let p;
-    for (let i = 0; i <= 2; i++) {
-        for (let j = 0; j <= 2; j++) {
-            ptX = line.tab[i][j].x;
-            ptY = line.tab[i][j].y;
-            distance = distanceEntreCoordonnees(mouseX, mouseY, ptX, ptY);
-            if(distance <= 100) {
-                p = new position(i, j);
-                return p;
-            }
-        }
-        
-    }
-}
-
-function verifiOccupe(posi, posj) {
-    if(shapes.length > 0){
-        for(let shape of shapes) {
-            if(shape.i == posi && shape.j == posj) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-function posePiece(mouseX, mouseY) {
-    // i, j du click
-    let pos = getPosition(mouseX, mouseY);
-    if(!verifiOccupe(pos.i, pos.j)) {
-        let id = "piece"+ nbrPieces;
-        if(parite(nbrPieces) && !testAlign(pos.i, pos.j, noir)) {
-            shapes.push(new pions(id, pos.i, pos.j, noir));
-            nbrPieces++;
-        } else if(!parite(nbrPieces) && !testAlign(pos.i, pos.j, vert)) {
-            shapes.push(new pions(id, pos.i, pos.j, vert));
-            nbrPieces++;
-        } else {
-            console.log("Probleme Align");
-        }
-    } else {
-        console.log("Occupe");
-    }
-}
-
 var gagne = false;
 
+document.getElementById("help").innerHTML = "Poser vos piéces chacun votre tour";
 var tour = noir;
 function canvasClick(event) {
     let plateauGame = canvas.getBoundingClientRect();
@@ -95,27 +49,30 @@ function canvasClick(event) {
     let mouseY = event.clientY - plateauGame.top;
 
     if(nbrPieces < 6) {
+        document.getElementById("help").innerHTML = "";
         posePiece(mouseX, mouseY);
+        if(gagne)
+                document.getElementById("winner").style.display = "flex";
     } else {
-        console.log('STOP');
+
         decolorage();
         if(!clicked) {
             idPiece = '';
             verifyCercle(mouseX, mouseY, tour);
         } else {
+            document.getElementById("help").innerHTML = "";
             clicked = false;
-            console.log("Mouvement amzay zao");
             posi = posj = null;
             moveCercle(idPiece, mouseX, mouseY);
+            console.log(gagne);
+            if(gagne)
+                document.getElementById("winner").style.display = "flex";
         }
     }
 
 }
 
 canvas.addEventListener("click", canvasClick);
-
-if(gagne)
-    canvas.removeEventListener("click", canvasClick);
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
